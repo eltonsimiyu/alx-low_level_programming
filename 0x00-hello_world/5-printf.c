035a333cb025c9c9c7153f8b60284239feec0d8e
eltonsimiyu <eltonsimiyu99@gmail.com>
2022-09-08 12:38:49 -0700

    correct no. 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
-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 */
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
