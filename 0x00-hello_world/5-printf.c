292569dfc4d7984882d562cb87864cbc124696ce
eltonsimiyu <eltonsimiyu99@gmail.com>
2022-09-08 12:17:59 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#include <stdio.h
-
 #include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+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 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
