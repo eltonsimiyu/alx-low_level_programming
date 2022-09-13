51e74987b16b4f2e24dcb0105c975117a6485490
eltonsimiyu <eltonsimiyu99@gmail.com>
2022-09-13 22:34:05 +0300

    First commit on functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include  "main.h"
+
+
+/**
+ * print_alphabet - print_alphabet
+ * Does my stuff explained.
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
