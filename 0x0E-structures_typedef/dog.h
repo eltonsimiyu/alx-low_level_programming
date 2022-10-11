78da0ab6cb37f9716fc903528503e60f233be5ae
eltonsimiyu <eltonsimiyu99@gmail.com>
2022-10-11 13:29:57 +0300

    Task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 #include <stdio.h>
-#include "dog.h"
+#include <stdlib.h>
 
 /**
 *struct dog - a new type of data
@@ -11,32 +11,19 @@
 *@age: age of dog
 *@owner: owner's name
 */
-
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-}
+char *name;
+float age;
+char *owner;
+};
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	struct dog my_dog;
-	my_dog.name = "Poppy";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-	return (0);
-}
+
 /**
 *dog_t - Typedef for struct dog
 */
 typedef struct dog dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
